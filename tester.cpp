750e5ecb3396c6fb2c1c0d706882998552020506
jm6ms <jm6ms@turing2.richmond.edu>
2018-04-02 14:43:59 -0400

    added test of Person.cpp

---
+++ tester.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Student.h"
+#include "Person.h"
+
+int main() 
+{
+	cout << "Testing setters/getters:" << endl;
+	cout << "-----------------------------------------------------" << endl << endl;
+	cout << "Person p;" << endl;
+	Person p;
+	cout << "p.setFirstName(\"Lilly\")" << endl;
+	p.setFirstName("Lilly");
+	cout << "First Name: " << p.getFirstName() << "[Expected: Lilly]" << endl;
+}
